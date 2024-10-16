335189378e416af54fe7042130347234168fcfc0
jova <130059240+jojohanb@users.noreply.github.com>
2024-10-16 11:55:28 -0300

    tem umas paradae pra arrumar

--- src/pages/tradutor/tradutor.jsx
+++ src/pages/tradutor/tradutor.jsx
@@ -3,26 +3,37 @@ import { useState } from "react";
 
 export function Tradutor(){
 
-    const [texto, setTexto] = useState("");
+     const [texto, setTexto] = useState("");
+     const [traduz, setTraduz] = useState("");
 
-    return (
-        <Pagina titulo="Tradutor" subtitulo="Tradutor Português-Inglês">
-            <div className="flex space-x-8">
-                <div className="flex flex-col">
-                    <h3>Português</h3>
-                    <textarea name="portugues" id="portugues" placeholder="Escreva algo..." className="border border-gray-300 rounded-lg w-44 h-24" maxLength={250} value={texto} onChange={(e)=> setTexto(e.target.value)}></textarea>
-                    <p className="text-white text-xs" >{texto.length}/250</p>
-                    <button className="mt-2 bg-blue-800 text-white border border-blue-950 rounded-lg px-2 py-2">Traduzir</button>
-                </div>
+     function traduzir() {
+        fetch( `https://api.mymemory.translated.net/get?q=${inputValue}&langpair=pt-br|en`)
+            .then((resposta) => resposta.json())
+            .then((dados) => setTraduz(dados.responseData.translatedText))
+            .catch((error) => {
+                alert("Erro:", error);
+            });
+        console.log(traduz);
+     }
+
+     return (
+         <Pagina titulo="Tradutor" subtitulo="Tradutor Português-Inglês">
+             <div className="flex space-x-8">
+                 <div className="flex flex-col">
+                     <h3>Português</h3>
+                     <textarea name="portugues" id="portugues" placeholder="Escreva algo..." className="border border-gray-300 rounded-lg w-44 h-24" maxLength={250} value={texto} onChange={(e)=> setTexto(e.target.value)} ></textarea>
+                     <p className="text-white text-xs" >{texto.length}/250</p>
+                     <button className="mt-2 bg-blue-800 text-white border border-blue-950 rounded-lg px-2 py-2"  onClick={traduzir}>Traduzir</button>
+                 </div>
   
-                <div className="flex flex-col">
-                    <h3>Inglês</h3>
-                    <div className="w-44 h-24 bg-white border border-gray-300 rounded-lg"></div>
-                </div>
-            </div>
+                 <div className="flex flex-col">
+                     <h3>Inglês</h3>
+                     <div className="w-44 h-24 bg-white border border-gray-300 rounded-lg">{traduz}</div>
+                 </div>
+             </div>
 
             
 
-       </Pagina>
-    )
-}
+        </Pagina>
+     )
+ }
